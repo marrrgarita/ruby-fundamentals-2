02588407ca1628f2d048e97085fc7802bb0e4936
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-28 16:17:19 -0400

    check the list for bananas

--- exercise9.rb
+++ exercise9.rb
@@ -11,3 +11,16 @@ grocery_list << "rice"
 groceries(grocery_list)
 
 puts grocery_list.length
+
+def check_list()
+  puts "What do you need to check on the list?"
+  item = gets.chomp
+  grocery_list=["carrots", "toilet paper", "apples", "salmon", "rice"]
+  if grocery_list.include?("item") == true
+    puts "you need to pick up #{item} today!"
+  else
+    puts "you don't need #{item} today!"
+  end
+end
+
+check_list()
