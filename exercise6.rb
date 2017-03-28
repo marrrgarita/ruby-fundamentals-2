72a5d9bc960e9459a0f891eeddd5d9f07e05e434
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-28 14:46:46 -0400

    output the result in a sentence

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,10 @@
 def temp_converter()
-  puts "What temp would you like to convert?"
+  puts "What Farhenheit tempurature would you like to convert to Celcius?"
   temp = gets.chomp
 
   new_temp = (temp.to_i - 32) * 5/9.0
+
+  puts "#{temp} degrees Farhenheit is equal to #{new_temp} degrees celcius"
 
 end
 
