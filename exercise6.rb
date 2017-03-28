ea3d9bc3508975444173b974e4ecc189d72c8368
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-28 17:20:04 -0400

    redo exercise 6 to ensure return value was not a string

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,19 @@
-def temp_converter()
-  puts "What Farhenheit tempurature would you like to convert to Celcius?"
-  temp = gets.chomp
+# def temp_converter()
+#   puts "What Farhenheit tempurature would you like to convert to Celcius?"
+#   temp = gets.chomp
+#
+#   new_temp = (temp.to_i - 32) * 5/9.0
+#
+#   puts "#{temp} degrees Farhenheit is equal to #{new_temp.to_i} degrees celcius"
+#
+# end
+#
+# temp_converter
 
-  new_temp = (temp.to_i - 32) * 5/9.0
-
-  puts "#{temp} degrees Farhenheit is equal to #{new_temp.to_i} degrees celcius"
-
+def f_to_c(temp)
+  temp_in_c = (temp - 32) * 5/9.0
 end
 
-temp_converter
+puts "Temp in F:"
+temp_in_f = gets
+puts "#{temp_in_f} degrees is #{f_to_c(temp_in_f.to_i)} degrees in Celcius"
