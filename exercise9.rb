39b8a399e9542f0ac8b2a620d72b8b416d602803
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-28 16:03:52 -0400

    Make grocery list and create method to output the list

---
+++ exercise9.rb
@@ -0,0 +1,9 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def groceries(grocery_list)
+  grocery_list.each do |item|
+    puts "* #{item}"
+  end
+end
+
+groceries(grocery_list)
